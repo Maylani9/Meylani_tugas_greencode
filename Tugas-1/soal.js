9a4ae75c85f69e2a9540a11d2be2d9c57b05ed67
meylani9 <meylani1214@gmail.com>
2023-10-04 16:30:08 +0700

    add tugas-1

---
+++ Tugas-1/soal.js
@@ -0,0 +1,13 @@
+let nama = "Maylani";
+let usia = 20;
+let tinggi = 155;
+let bilangan1 = 9;
+let bilangan2 = 2;
+let bilangan3 = 19;
+let isMahasiswa = true;
+let isMahasiswa1 = false;
+console.log(isMahasiswa1);
+console.log(isMahasiswa);
+console.log("hasil penjumlahan =", bilangan1 + bilangan2);
+console.log("hasil pengurangan=", bilangan2 - bilangan3);
+console.log("usia dan tinggi=", usia * tinggi);
