457fa7dcf09cf6ac964fb22f4125ec8d926c2b63
meylani9 <meylani1214@gmail.com>
2023-10-11 15:32:12 +0700

    add tugas2

--- Tugas-1/Tugas-2/tugas2_1.js
+++
@@ -1,7 +0,0 @@
-for (let index = 2; index <= 10; index++) {
-  if (index % 2 == 0) {
-    console.log(index, "Bilangan genap");
-  } else {
-    console.log(index, "Bilangan ganjil");
-  }
-}
---
+++ Tugas-2/tugas2_1.js
@@ -0,0 +1,7 @@
+for (let index = 1; index <= 10; index++) {
+  if (index > 10) {
+    console.log(index, "lebih dari 10");
+  } else {
+    // console.log(index, "Bilangan ganjil");
+  }
+}
