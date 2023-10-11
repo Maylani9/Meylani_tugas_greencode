85fd1c33138a4315733017ee1dd3f33d8274d5cf
meylani9 <meylani1214@gmail.com>
2023-10-11 18:03:06 +0700

    add tugas3

---
+++ Tugas 3/tugas 3.js
@@ -0,0 +1,13 @@
+function hitungLuasSegitiga(alas, tinggi) {
+  return (1 / 2) * alas * tinggi;
+}
+let varHitungLuasSegitiga = hitungLuasSegitiga(0.7, 0.8);
+console.log(varHitungLuasSegitiga);
+
+//function LuasSegitiga(alas,tinggi){
+// let rumus = 1/2 * alas * tinggi
+// return rumus
+
+//
+
+//rumus lain
