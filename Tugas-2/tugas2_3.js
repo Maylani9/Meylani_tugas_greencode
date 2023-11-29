ffdc10d924e2f526fd611f7a70c4bc3fb5526536
meylani9 <meylani1214@gmail.com>
2023-11-29 11:40:11 +0700

    add pr

--- Tugas-2/tugas2_3.js
+++ Tugas-2/tugas2_3.js
@@ -1,10 +1,17 @@
-let nilaiku = 75;
-if (nilaiku > 90) {
-  console.log("nilai sangat bagus");
-} else if (nilaiku < 90 && nilaiku > 80) {
-  console.log("nilai  bagus");
-} else if (nilaiku < 80 && nilaiku > 70) {
-  console.log("nilai cukup bagus");
-} else if (nilaiku < 70 && nilaiku > 0) {
-  console.log("nilai buruk");
+// let nilaiku = 75;
+// if (nilaiku > 90) {
+//   console.log("nilai sangat bagus");
+// } else if (nilaiku < 90 && nilaiku > 80) {
+//   console.log("nilai  bagus");
+// } else if (nilaiku < 80 && nilaiku > 70) {
+//   console.log("nilai cukup bagus");
+// } else if (nilaiku < 70 && nilaiku > 0) {
+//   console.log("nilai buruk");
+// }
+
+let isMahasiswa = false;
+if (isMahasiswa == false) {
+  console.log("aku  bukan mahasiswa");
+} else if (isMahasiswa == true) {
+  console.log("aku  mahasiswa");
 }
---
+++ pr/main.js
@@ -0,0 +1,66 @@
+//Soal no 1 Menghitung Luas Persegi Panjang//
+let panjang = 5;
+let lebar = 8;
+let luas = panjang * lebar;
+console.log(luas);
+
+//soal no 2 Menghitung Ratarata 2 Angka//
+let angka1 = 10;
+let angka2 = 20;
+let rataRata = (angka1 + angka2) / 2;
+console.log(rataRata);
+
+//Soal no 3 Menghitung Luas Segitiga//
+
+let tinggi = 8;
+let alas = 12;
+let luasSegitiga = (alas * tinggi) / 2;
+console.log(luasSegitiga);
+
+//soal no 4 Menghitung Sisa Hasil Bagi//
+let angka3 = 24;
+let angka4 = 5;
+let sisaHasilBagi = angka3 % angka4;
+console.log(sisaHasilBagi);
+
+//soal no 5 Mengkonversi Waktu//
+let detik = 120;
+let menit = 60;
+let hasilKonversi = detik / menit;
+console.log(hasilKonversi);
+
+//soal no 6 Menghitung Keliling Persegi dengan Perkalian dan Penjumlahan//
+let sisi = 9;
+let sisiKeliling = 4 * sisi;
+let sisiKeliling1 = sisi + sisi + sisi + sisi;
+console.log(sisiKeliling);
+console.log(sisiKeliling1);
+
+//soal no 7 Menghitung Total Waktu//
+let tugas1 = 9;
+let tugas2 = 2;
+let waktuTugas = tugas1 + tugas2;
+console.log(waktuTugas);
+
+//soal no 8 Menghitung Volume Silinder//
+let phi = 3.14;
+let jari = 9;
+let tinggiVolume = 24;
+let volume = phi * (jari * 2) * tinggiVolume;
+console.log(volume);
+
+//soal no 9 Mengitung rata rata 3 angka menggunakan pembagian dan perjumlahan//
+let angka5 = 9;
+let angka6 = 2;
+let angka7 = 19;
+let angka8 = 5;
+let rataAngka = angka5 + angka6 + angka7 / angka8;
+console.log(rataAngka);
+
+//soal no 10 Mengitung rata rata 3 angka menggunakan pembagian dan perjumlahan//
+let angka9 = 100;
+let angka10 = 200;
+let angka11 = 300;
+let angka12 = 400;
+let rataAngka1 = angka9 + angka10 + angka11 / angka12;
+console.log(rataAngka1);
